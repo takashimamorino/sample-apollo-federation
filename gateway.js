cda3c312695b7f898bbc8afb9928820137f41e86
takashimamorino <does1026@gmail.com>
2021-02-07 19:40:30 +0900

    fix

---
+++ gateway.js
@@ -0,0 +1,17 @@
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway } = require('@apollo/gateway');
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: 'tickets', url: 'http://localhost:4001' },
+  ],
+});
+
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+});
+
+server.listen(4000).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
