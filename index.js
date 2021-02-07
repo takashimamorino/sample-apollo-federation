99d35f3c87228f7ba0cb3a888d9574f155c9fbd5
takashimamorino <does1026@gmail.com>
2021-02-07 18:41:17 +0900

    create inde.

---
+++ index.js
@@ -0,0 +1,29 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    id: ID!
+    username: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me() {
+      return { id: "1", username: "@ava" }
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen(4001).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
